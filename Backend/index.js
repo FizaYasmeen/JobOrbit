d233f2c7efcbe9f5a47b9691c6b27286200c09db
Fiza Yasmeen <152010285+FizaYasmeen@users.noreply.github.com>
2025-08-09 23:07:01 +0530

    Update server.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,36 @@
-import mongoose from "mongoose";
+import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import 'dotenv/config'
+import userRoute from './routes/user.route.js'
+import { connectDB } from './db/index.js'
+import companyRoute from './routes/company.route.js'
+import jobRoute from './routes/job.route.js'
+import applicationRoute from './routes/application.route.js'
+const app = express()
 
-export const connectDB = async ()=>{
-    try{
-        await mongoose.connect(process.env.MONGODB_URL)
-    }catch(err){
-        console.log(err)
-    }
+const corsOptions = {
+    origin: true, 
+    credentials: true, 
 }
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+app.use(cors(corsOptions))
+
+app.use('/api/v1/user',userRoute)
+app.use('/api/v1/company',companyRoute)
+app.use('/api/v1/job',jobRoute)
+app.use('/api/v1/application',applicationRoute)
+
+app.listen(process.env.PORT,()=>{
+    connectDB()
+    .then(()=>{
+        console.log("database connected")
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+    console.log(`Server is running on ${process.env.PORT}`)
+})
